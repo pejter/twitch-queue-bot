d09672066eb8229c9cddbf697eebfd261447e827
Piotr Łuczak <piotrluczak1995@gmail.com>
2021-09-11 01:12:41 +0200

    Adaptive rate limits based on mod status

--- src/lib/mod.rs
+++ src/lib/mod.rs
@@ -1,16 +1,19 @@
 mod ratelimit;
 
+use ratelimit::Limiter;
 use std::collections::HashSet;
 use std::error::Error;
 use std::io;
 use std::io::prelude::*;
 use std::net::TcpStream;
 use std::sync::mpsc::{channel, SendError, Sender};
+use std::sync::{Arc, RwLock};
 use std::time::Duration;
 
 pub type ChannelContent = String;
 pub type ChannelError = SendError<ChannelContent>;
 
+const USER_RATE_LIMIT: usize = 20;
 const MOD_RATE_LIMIT: usize = 100;
 
 #[derive(Clone)]
@@ -24,6 +27,7 @@ pub struct ChatClient {
     config: ChatConfig,
     socket: TcpStream,
     sender: Sender<ChannelContent>,
+    limiter: Arc<RwLock<Limiter>>,
     pub modlist: HashSet<String>,
 }
 
@@ -37,12 +41,14 @@ impl ChatClient {
         let socket = TcpStream::connect("irc.chat.twitch.tv:6667")?;
         let mut socket_recv = socket.try_clone()?;
         let (sender, receiver) = channel();
-
+        let limiter = Arc::new(RwLock::new(Limiter::new(
+            USER_RATE_LIMIT,
+            Duration::from_secs(30),
+        )));
+        let limiter_inner = Arc::clone(&limiter);
         std::thread::spawn(move || {
-            let mut limiter = ratelimit::Limiter::new(MOD_RATE_LIMIT, Duration::from_secs(30));
-
             for msg in receiver.iter() {
-                limiter.wait();
+                limiter_inner.write().unwrap().wait();
                 println!("Sending: {}", msg);
                 socket_recv
                     .write_all(format!("{}\r\n", msg).as_bytes())
@@ -54,6 +60,7 @@ impl ChatClient {
             socket,
             config,
             sender,
+            limiter,
             modlist: HashSet::new(),
         };
 
@@ -87,6 +94,13 @@ impl ChatClient {
         self.modlist.clear();
         self.modlist.insert(chan.to_owned());
         self.modlist.extend(modlist.map(|s| s.to_owned()));
+        match self
+            .modlist
+            .contains(&self.config.bot_username.to_lowercase())
+        {
+            true => self.limiter.read().unwrap().set_capacity(MOD_RATE_LIMIT),
+            false => self.limiter.read().unwrap().set_capacity(USER_RATE_LIMIT),
+        }
     }
 }
 
--- src/lib/ratelimit.rs
+++ src/lib/ratelimit.rs
@@ -1,10 +1,12 @@
 use std::collections::VecDeque;
 use std::ops::Add;
+use std::sync::atomic::{AtomicUsize, Ordering};
 use std::time::{Duration, Instant};
 
 pub struct Limiter {
     interval: Duration,
-    tokens: usize,
+    capacity: AtomicUsize,
+    tokens: AtomicUsize,
     window: VecDeque<Instant>,
 }
 
@@ -13,29 +15,37 @@ impl Limiter {
         if capacity == 0 {
             panic!("Capacity can't be zero!")
         }
+
         Self {
             interval,
-            tokens: capacity,
             window: VecDeque::new(),
+            tokens: AtomicUsize::new(capacity),
+            capacity: AtomicUsize::new(capacity),
         }
+    }
+
+    pub fn set_capacity(&self, new_cap: usize) {
+        let old_cap = self.capacity.swap(new_cap, Ordering::SeqCst);
+        self.tokens.fetch_add(new_cap - old_cap, Ordering::SeqCst);
     }
 
     pub fn refill(&mut self) {
         let now = Instant::now();
         let num_expired = self.window.partition_point(|&i| i < now);
         self.window = self.window.split_off(num_expired);
-        self.tokens += num_expired;
+        // This might panic if we ever get unmodded while sending a lot of messages
+        self.tokens.fetch_add(num_expired, Ordering::SeqCst);
     }
 
     pub fn take(&mut self) {
         self.window.push_back(Instant::now().add(self.interval));
-        self.tokens -= 1
+        self.tokens.fetch_sub(1, Ordering::SeqCst);
     }
 
     pub fn wait(&mut self) {
         loop {
             self.refill();
-            if self.tokens == 0 {
+            if self.tokens.load(Ordering::Acquire) == 0 {
                 let wait_time = match self.window.get(0) {
                     Some(future) => future.saturating_duration_since(Instant::now()),
                     None => Duration::from_millis(100), // This should never happen
