c97fe6eef86fcb42102b65c99d142f3fd3a4c336
Piotr Łuczak <piotrluczak1995@gmail.com>
2021-09-10 05:16:52 +0200

    Refactor config reading

--- src/config.rs
+++ src/config.rs
@@ -1,25 +1,19 @@
 use std::collections::HashMap;
 use std::fs::File;
 use std::io::prelude::*;
-use std::io::{BufReader, Error, ErrorKind};
+use std::io::{BufReader, Error};
 
 pub fn read() -> Result<HashMap<String, String>, Error> {
     let file = File::open("config.txt")?;
     let reader = BufReader::new(file);
 
-    let mut map = HashMap::new();
-    for line in reader.lines() {
+    Ok(HashMap::from_iter(reader.lines().map(|line| {
         let line = line.unwrap();
         let results: Vec<&str> = line.splitn(2, '=').collect();
-        match results.len() {
-            2 => {
-                map.insert(results[0].trim().to_string(), results[1].trim().to_string());
-            }
-            _ => {
-                return Err(Error::new(ErrorKind::Other, "Malformed config"));
-            }
+        if results.len() == 2 {
+            (results[0].trim().to_string(), results[1].trim().to_string())
+        } else {
+            panic!("Malformed config entry '{}'", line);
         }
-    }
-
-    Ok(map)
+    })))
 }
